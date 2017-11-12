e36e92122300ddee76b5487fe173d58acca7e798
ReticulatedSpline <carpenter.ben@icloud.com>
2017-11-11 18:59:47 -0600

    Submitted card UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -53,16 +53,23 @@ export class AppComponent {
   valid() : boolean {
     for (let regex of this.regexes) {
       if (regex.name === "" ||
-          regex.expr === "" ||
-          this.root === "") {
+          regex.expr === "") {
         return false;
       }
     }
-    return true;
+    if (this.root === "") {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  onKey(event: "rootTrigger") {
+    this.valid();
   }
 
   submit() : void {
-    //parse each webpage for regex matches
+    this.submitted = true;
     }
 
   setMailTo() : void {
@@ -76,12 +83,13 @@ export class AppComponent {
       duration: 2000,
     });
   }
-
   public githubLogoPath = "https://assets-cdn.github.com/favicon.ico";
   public questionMarkLogoPath = "http://www.act.org/content/dam/act/unsecured/Images/icons/icon-question.png";
   public githubLink = "https://github.com/ReticulatedSpline/Web-Crawler";
   public regexLink = "https://www.w3schools.com/jsref/jsref_obj_regexp.asp"
-  private emailExpr = "^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
-  private phoneExpr = "^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$";
-  private addressExpr = "\d{1,5}\s\w.\s(\b\w*\b\s){1,2}\w*\.";
 }
+
+const emailExpr = "^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
+const phoneExpr = "^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$";
+const addressExpr = "\d{1,5}\s\w.\s(\b\w*\b\s){1,2}\w*\.";
+const urlRegex = "[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?;"
